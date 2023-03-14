f63717ca71ae8b5c5efd6b7bec7a084c9dd4153f
Ivan Vasilev <tor419@yandex.ru>
2023-03-14 12:51:05 +0700

    Added four test commit

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -21,17 +21,6 @@ public class Player {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Map<Game, Integer> getPlayedTime() {
-        return playedTime;
-    }
-
-    public void setPlayedTime(Map<Game, Integer> playedTime) {
-        this.playedTime = playedTime;
-    }
 
     /**
      * добавление игры игроку
